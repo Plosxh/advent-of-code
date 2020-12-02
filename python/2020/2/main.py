ad366f87a6c66f5c32e4502a2de6ec9a825b79ad
Pierre Charrat <pierre.charrat@teamwork.net>
2020-12-02 16:19:21 +0100

    add day 2

--- python/2020/2/main.py
+++ python/2020/2/main.py
@@ -1,9 +1,31 @@
 
-def main(inputs):
-    print(inputs)
+
+
+def  main(inputs):
+    valid_password = 0
+    for input in inputs:
+        split = input.split(" ")
+        plit = input.split(" ")
+        first, second = split[0].split("-")
+        policy = split[1][:-1]
+        password = split[len(split) - 1]
+        if (password[int(first)-1] ==policy) != (password[int(second)-1] == policy):
+            valid_password +=1
+    print(valid_password)
+
+
+def main_part_1(inputs):
+    valid_password = 0
+    for input in inputs:
+        split = input.split(" ")
+        min, max = split[0].split("-")
+        policy = split[1][:-1]
+        password = split[len(split)-1]
+        if int(min) <= password.count(policy) <= int(max):
+            valid_password += 1
+    print(valid_password)
 
 
 if __name__ == '__main__':
     with open("./inputs.txt","r") as file:
         main(file.read().splitlines())
-    main()
